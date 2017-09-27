133fd1f88eb6250fd37252841afc7e70b88b2ae3
John Michael and Nate McGregor <nate@>
2017-09-27 13:23:22 -0700

    pass ingredient rspec

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -3,9 +3,10 @@ require('spec_helper')
 describe('Ingredient') do
   describe("#ingredients") do
   it("tells the name of recipe the ingredient belongs to") do
-    test_recipe = Recipe.create({:recipe_name => 'Water Soup', :instruction => 'Add water and bring to a lower simmer', :rating => 10, :tag_id =>nil })
-    test_ingredient = Ingredient.create({:ingredient => "Dehydrated Water", :recipe_id => test_recipe.id})
-    expect(test_ingredient.recipe()).to(eq(test_recipe))
+    test_recipe = Recipe.create({:recipe_name => 'Water Soup', :rating => 10})
+    test_ingredient = Ingredient.create({:item => "Dehydrated Water", :amount => "1 cup"})
+    test_rjoin = Rjoin.create({:recipe_id => test_recipe.id, :ingredient_id => test_ingredient.id, :tag_id => 1, :step_id => 1})
+    expect(test_ingredient.recipes()).to(eq([test_recipe]))
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,9 @@ require("sinatra/activerecord")
 require("recipe")
 require("tag")
 require("ingredient")
+require("step")
+require("rjoin")
+require("pry")
 
 
 RSpec.configure do |config|
@@ -25,6 +28,18 @@ RSpec.configure do |config|
   config.after(:each) do
     Recipe.all().each() do |recipe|
       recipe.destroy()
+    end
+  end
+
+  config.after(:each) do
+    Step.all().each() do |step|
+      step.destroy()
+    end
+  end
+
+  config.after(:each) do
+    Rjoin.all().each() do |rjoin|
+      rjoin.destroy()
     end
   end
 end
